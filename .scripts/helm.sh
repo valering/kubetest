06935201ed2e0c373e464691f06ca1ec993f0738
Valery Kobyzev <welldevops@gmail.com>
2017-10-23 12:58:19 -0700

    debug

--- .scripts/helm.sh
+++ .scripts/helm.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
+echo $KUBECONFIG
+
 # create and set path for our bin dir
 PATH=$PATH:$(pwd)/bin
-export KUBECONFIG=$KUBECONFIG
+export KUBECONFIG=$CIRCLE_WORKING_DIRECTORY/.kube/config
 
 helm list
