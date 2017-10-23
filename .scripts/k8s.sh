79f83519a805a64fd00f25a00d44e99eceb25655
Valery Kobyzev <welldevops@gmail.com>
2017-10-23 12:51:35 -0700

    debug

--- .scripts/k8s.sh
+++ .scripts/k8s.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -x
 
-mkdir $WORKDIR/bin
+mkdir $CIRCLE_WORKING_DIRECTORY/bin
 
 # install curl
 apt-get update && apt-get -y install curl
 
 # create and set path for our bin dir
-mkdir $WORKDIR/bin
+mkdir $CIRCLE_WORKING_DIRECTORY/bin
 PATH=$PATH:$(pwd)/bin
 
 # install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.8/bin/linux/amd64/kubectl
 chmod +x kubectl
-mv kubectl $WORKDIR/bin
+mv kubectl $CIRCLE_WORKING_DIRECTORY/bin
 
 # install helm
 curl -LO https://storage.googleapis.com/kubernetes-helm/helm-v2.6.1-linux-amd64.tar.gz
 tar xzf helm-v2.6.1-linux-amd64.tar.gz
-mv linux-amd64/helm $WORKDIR/bin
+mv linux-amd64/helm $CIRCLE_WORKING_DIRECTORY/bin
 
 # configure k8s
-mkdir $WORKDIR/.kube
-echo $KUBECONFDATA_DEV | base64 --decode --ignore-garbage > $WORKDIR/.kube/config
+mkdir $CIRCLE_WORKING_DIRECTORY/.kube
+echo $KUBECONFDATA_DEV | base64 --decode --ignore-garbage > $CIRCLE_WORKING_DIRECTORY/.kube/config
